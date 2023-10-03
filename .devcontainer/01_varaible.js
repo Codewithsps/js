bb22d4f5e164292a6499a445b5c3c0b8edc1d31a
SP Singh <96386524+Codewithsps@users.noreply.github.com>
2023-10-04 00:53:33 +0530

    Update 01_varaible.js

--- .devcontainer/01_varaible.js
+++ .devcontainer/01_varaible.js
@@ -1,10 +1,15 @@
+const account_id = 12345
+let account_email = "Codewithsps.github.io"
+var accountPassword ="12345@"
+accountCity = "New Delhi"
+let accountState;
+// account_id = 5  // not allowed
+// account_id = 5  // not allowed
 
-# Please enter the commit message for your changes. Lines starting
-# with '#' will be ignored, and an empty message aborts the commit.
-#
-# On branch main
-# Your branch is up to date with 'origin/main'.
-#
-# Changes to be committed:
-#	new file:   .devcontainer/01_varaible.js
-#
+
+console.log(account_id);
+account_email = "SPSingh@gmail.com"
+accountPassword = "6789@"
+accountCity = "Noida"
+console.table([ account_id, account_email, accountPassword, accountCity, accountState])
+/* prefer not use to var because off issue in block scope functional scope */
